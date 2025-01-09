61a9fb7605b19683483f7667a9393d3201af443c
Cgeorge1807 <christian03george@gmail.com>
2025-01-09 18:53:03 +0000

    Valores Aleatorios

--- src/app.js
+++ src/app.js
@@ -8,4 +8,17 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "K", "Q"];
+  let numAleatorio = numeros[Math.floor(Math.random() * numeros.length)];
+  console.log(numAleatorio);
+  let palos = ["♦", "♥", "♠", "♣"];
+  let palosAleatorio = palos[Math.floor(Math.random() * palos.length)];
+  console.log(palosAleatorio);
+  document.getElementById("palo").innerHTML = palosAleatorio;
+  document.getElementById("palo-abajo").innerHTML = palosAleatorio;
+  document.getElementById("numero").innerHTML = numAleatorio;
+  if (palosAleatorio == "♥" || palosAleatorio == "♦") {
+    document.getElementById("palo").style.color = "red";
+    document.getElementById("palo-abajo").style.color = "red";
+  }
 };
