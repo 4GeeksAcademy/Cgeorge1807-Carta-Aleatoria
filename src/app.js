7e02e29335560da14bce83197df11824d46e6b46
Cgeorge1807 <christian03george@gmail.com>
2025-01-09 20:40:30 +0000

    Graficos terminados

--- src/app.js
+++ src/app.js
@@ -22,3 +22,16 @@ window.onload = function() {
     document.getElementById("palo-abajo").style.color = "red";
   }
 };
+// window.onload = function() {
+//   let numeros = [2, 3, 4, 5, 6, 7, 8, 9, 10, "A", "J", "K", "Q"];
+//   let numAleatorio = numeros[Math.floor(Math.random() * numeros.length)];
+//   let palos = ["♦", "♥", "♠", "♣"];
+//   let palosAleatorio = palos[Math.floor(Math.random() * palos.length)];
+//   document.getElementById("palo").innerHTML = palosAleatorio;
+//   document.getElementById("palo-abajo").innerHTML = palosAleatorio;
+//   document.getElementById("numero").innerHTML = numAleatorio;
+//   if (palosAleatorio == "♥" || palosAleatorio == "♦") {
+//     document.getElementById("palo").style.color = "red";
+//     document.getElementById("palo-abajo").style.color = "red";
+//   }
+// };
